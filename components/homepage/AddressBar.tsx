b88fbbdc6804313c24e2bd22f6fd71254493b98a
Shivam <shivam.sagar@magicpin.in>
2024-06-16 16:21:00 +0530

    Changes

--- components/homepage/AddressBar.tsx
+++ components/homepage/AddressBar.tsx
@@ -6,7 +6,7 @@ const AddressBar = () => {
         <div className="bg-[#F5F5F5] flex mx-4 py-3 px-5 h-fit rounded-full items-center gap-2 justify-between ">
             <div className="flex gap-2 items-center">
                 <GiPadlock />
-                <p> https://magicpin.in/ </p>
+                <p> https://unsplash.com/ </p>
             </div>
             <ImNewTab className="cursor-pointer" />
         </div>
